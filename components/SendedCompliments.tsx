d54f4754ccd87f880fd6a7fe7a6c529d15719007
Teilor Souza Barcelos <tsb-@live.com>
2021-08-23 16:44:00 -0300

    compliments refatored without context

--- components/SendedCompliments.tsx
+++ components/SendedCompliments.tsx
@@ -1,22 +1,80 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { AuthContext } from "../contexts/AuthContext"
-import { ComplimentsContext } from "../contexts/ComplimentsContext"
 
 export default function SendedCompliments() {
     const { auth } = useContext(AuthContext)
-    const {get} = useContext(ComplimentsContext)
+    let compliments: any = undefined
+    let val
 
-    if(auth){
-        const SendedCompliments = get(auth.token)
+    async function getCompliments(){
+
+        const response = await fetch('https://valorize.herokuapp.com/compliments/list', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${auth?.token}`
+            }
+    
+        })
+    
+        const result = await response.json()
+    
+        if (await result.error) {
+            alert(result.error)
+            return
+        }
+        
+        compliments = result
+
+        // console.log(compliments.complimentsSend)
+
     }
 
+    getCompliments().then(() => {
 
+        if(compliments){
+            console.log(compliments.complimentsSend)
+        }
+
+    })
 
     return (
-        <div className="sended-complaiments">
-
-
-
-        </div>
+        <>
+        
+            
+        
+        </>
     )
+
+    // const { compliments } = useContext(ComplimentsContext)
+
+    // let sendedCompliments = null
+
+    // if (compliments && compliments.complimentsSend.length > 0) {
+    //     sendedCompliments = compliments.complimentsSend.map((compliment: ICompliment | null) => {
+    //         return (
+    //             <div key={compliment?.id} className="sended-compliment">
+    //                 <h4>#{compliment?.message}</h4>
+    //             </div>
+    //         )
+    //     })
+    // }
+
+    // return (
+    //     <>
+    //         {sendedCompliments &&
+    //             <>
+    //                 <div className="sended-complaiments">
+    //                     <h4>
+    //                         Elogios enviados
+    //                     </h4>
+
+    //                     {sendedCompliments}
+
+
+    //                 </div>
+    //             </>
+    //         }
+    //     </>
+    // )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,11 @@ import '../styles/_spacing.scss'
 import '../styles/_typography.scss'
 import '../styles/_layout.scss'
 import '../styles/globals.scss'
-import { ComplimentsProvider } from '../contexts/ComplimentsContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <ComplimentsProvider>
-        <Component {...pageProps} />
-      </ComplimentsProvider>
+      <Component {...pageProps} />
     </AuthProvider>
   )
 }
